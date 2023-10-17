c19ffb7c3cc60139d55f9560c699cc5ecb86fbce
Baxa <baxtiyornarziev6@gmail.com>
2023-10-17 22:30:46 +0300

    new

--- src/Component/Context/Spotify.CTX.js
+++ src/Component/Context/Spotify.CTX.js
@@ -1,9 +1,14 @@
 import { createContext } from "react";
 
+
+ const actualURL = import.meta.url.split('/src')[0]
+
+ console.log(actualURL);
 export const spotify = createContext({
     client_id: import.meta.env.VITE_API_KEY,
     user_id: import.meta.env.VITE_API_USER_ID,
-    REDIRECT_URI: 'http://localhost:5173',
+    REDIRECT_URI: actualURL,
+    
     AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
     RESPONSE_TYPE: 'token',
     SCOPES : ["user-read-currently-playing", "user-follow-read", "user-read-email", "user-read-private","user-library-read", "user-read-recently-played", "playlist-modify-public", "playlist-modify-private", "playlist-read-private", "user-top-read"]
