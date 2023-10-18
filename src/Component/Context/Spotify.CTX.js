4d66c6160e0414ea46b3e98538a73717a1f6353e
Baxa <baxtiyornarziev6@gmail.com>
2023-10-18 21:28:26 +0300

    new-commit

--- src/Component/Context/Spotify.CTX.js
+++ src/Component/Context/Spotify.CTX.js
@@ -3,7 +3,7 @@ import { createContext } from "react";
 
 //  const actualURL = import.meta.url.split('/src')[0]
 
- console.log(actualURL);
+
 export const spotify = createContext({
     client_id: import.meta.env.VITE_API_KEY,
     user_id: import.meta.env.VITE_API_USER_ID,
