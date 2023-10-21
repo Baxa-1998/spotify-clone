c953aee6de7190a4c083bdc613e29096b6eca6b0
Baxa <baxtiyornarziev6@gmail.com>
2023-10-21 15:45:59 +0300

    new

--- src/Component/Context/Spotify.CTX.js
+++ src/Component/Context/Spotify.CTX.js
@@ -1,16 +1,24 @@
-import { createContext } from "react";
-
+import { createContext } from 'react';
 
 //  const actualURL = import.meta.url.split('/src')[0]
 
-
 export const spotify = createContext({
-    client_id: import.meta.env.VITE_API_KEY,
-    user_id: import.meta.env.VITE_API_USER_ID,
-    REDIRECT_URI: import.meta.env.VITE_AUTH_URL,
-    
-    AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
-    RESPONSE_TYPE: 'token',
-    SCOPES : ["user-read-currently-playing", "user-follow-read", "user-read-email", "user-read-private","user-library-read", "user-read-recently-played", "playlist-modify-public", "playlist-modify-private", "playlist-read-private", "user-top-read"]
- 
-})
+  client_id: import.meta.env.VITE_API_KEY,
+  user_id: import.meta.env.VITE_API_USER_ID,
+  REDIRECT_URI: import.meta.env.VITE_REDIRECT_URL,
+
+  AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
+  RESPONSE_TYPE: 'token',
+  SCOPES: [
+    'user-read-currently-playing',
+    'user-follow-read',
+    'user-read-email',
+    'user-read-private',
+    'user-library-read',
+    'user-read-recently-played',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'playlist-read-private',
+    'user-top-read',
+  ],
+});
